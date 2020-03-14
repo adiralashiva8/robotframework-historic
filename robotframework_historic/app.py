a0580a21e7e50092fdddd8990564262be97bfdc7
Tommy H <hoangx@users.noreply.github.com>
2020-03-14 04:00:28 -0500

    Update app.py

--- robotframework_historic/app.py
+++ robotframework_historic/app.py
@@ -288,7 +288,7 @@ def main():
     args = parse_options()
 
     app.config['MYSQL_HOST'] = args.sqlhost
-    app.config['MYSQL_PORT'] = args.sqlport
+    app.config['MYSQL_PORT'] = str(args.sqlport)
     app.config['MYSQL_USER'] = args.username
     app.config['MYSQL_PASSWORD'] = args.password
     app.config['auth_plugin'] = 'mysql_native_password'
