691f77809c0f8b7d9785689cdfd1b30cdd4e5543
Tommy H <hoangx@users.noreply.github.com>
2020-03-14 05:43:14 -0500

    Update app.py

--- robotframework_historic/app.py
+++ robotframework_historic/app.py
@@ -7,9 +7,13 @@ app = Flask(__name__, template_folder='templates')
 
 mysql = MySQL(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    use_db(cursor, "robothistoric")
+    cursor.execute("select * from TB_PROJECT;")
+    data = cursor.fetchall()
+    return render_template('index.html', data=data)
 
 @app.route('/index', methods=['GET'])
 def index():
