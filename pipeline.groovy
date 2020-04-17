8aab5644c192904d0d308d3727d29e7e70181245
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 13:36:06 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,19 +1,14 @@
 pipeline {
     agent {label 'tommy_docker'}
-    
-      stage('Clone repository') {
-        checkout scm
-      }
-      
-      stage('Build image') {
-              app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
-          }
-          
-          stage('Push image') {
-              docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
-                  app.push("${env.BUILD_NUMBER}")
-                  app.push("latest")
-                  }
-                      echo "Push Docker Build to Proget"
-          }
-}
+    stages {
+                stage("Pull Repository")
+                {
+                    agent { label "tommy_docker" }
+                    steps {
+                        script{container('qe-docker') {
+                        git branch: 'master', url: 'git@ithub.com/Accruent/robotframework-historic.git'
+                        }}
+                    }
+                }
+            }
+        }
