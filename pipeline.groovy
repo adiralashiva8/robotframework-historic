a1584d9612aa76693a1f26c0da71f6addd111112
tommyhoang1 <tommy.hoang@accruent.com>
2020-04-16 22:15:14 -0500

    added pipeline

---
+++ pipeline.groovy
@@ -0,0 +1,33 @@
+node {
+    def app
+
+    stage('Clone repository') {
+        /* Cloning the Repository to our Workspace */
+
+        checkout scm
+    }
+
+    stage('Build image') {
+        /* This builds the actual image */
+
+        app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
+    }
+
+    stage('Test image') {
+
+        app.inside {
+            echo "Tests passed"
+        }
+    }
+
+    stage('Push image') {
+        /*
+			You would need to first register with DockerHub before you can push images to your account
+		*/
+        docker.withRegistry('proget.accruentsystems.com', 'svcselenium') {
+            app.push("${env.BUILD_NUMBER}")
+            app.push("latest")
+            }
+                echo "Trying to Push Docker Build to DockerHub"
+    }
+}
