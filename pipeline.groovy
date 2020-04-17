5ab6cdcff852c69c184dde44dd06b63c74da58fd
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 07:19:20 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,14 +12,14 @@ node {
 
         app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
     }
-
+/*
     stage('Test image') {
 
         app.inside {
             echo "Tests passed"
         }
     }
-
+*/
     stage('Push image') {
         /*
 			You would need to first register with DockerHub before you can push images to your account
