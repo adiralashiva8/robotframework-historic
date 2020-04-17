252b9f327de60ae8450eb8e891defa4457d36903
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 15:06:43 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,6 @@ pipeline {
     agent {label 'tommy_test'}
     stages {
                 stage("Pull Repository") {
-                    agent { label "tommy_test" }
                     steps {
                         script{container('qe-docker') {
                         git branch: 'master', url: 'https://github.com/Accruent/robotframework-historic.git'
@@ -11,7 +10,6 @@ pipeline {
                 }
 	    
 		stage('Build image') {
-			agent { label "tommy_test" }
                     	steps {
 				script{container('qe-docker') {
 				def app	
@@ -21,7 +19,6 @@ pipeline {
 		}
 
 		stage('Push image') {
-			agent { label "tommy_test" }
                     	steps {
 				script{container('qe-docker') {
 				docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
