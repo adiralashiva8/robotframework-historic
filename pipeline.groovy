1f900de3530e0042fd326743a2fd794dd7f5c1d0
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 13:37:27 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,9 @@
 pipeline {
-    agent {label 'tommy_docker'}
+    agent {label 'tommy_test'}
     stages {
                 stage("Pull Repository")
                 {
-                    agent { label "tommy_docker" }
+                    agent { label "tommy_test" }
                     steps {
                         script{container('qe-docker') {
                         git branch: 'master', url: 'git@ithub.com/Accruent/robotframework-historic.git'
