714175de148927651692a9a7bae9302596fcd98e
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 13:31:23 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,26 +1,20 @@
-node {
+pipeline {
+    agent {label 'tommy_docker'}
     def app
-
-    stage('Clone repository') {
+    
+      stage('Clone repository') {
         checkout scm
-    }
-
-    stage('Build image') {
-        app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
-    }
-/*
-    stage('Test image') {
-
-        app.inside {
-            echo "Tests passed"
-        }
-    }
-*/
-    stage('Push image') {
-        docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
-            app.push("${env.BUILD_NUMBER}")
-            app.push("latest")
-            }
-                echo "Trying to Push Docker Build to Proget"
-    }
+      }
+      
+      stage('Build image') {
+              app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
+          }
+          
+          stage('Push image') {
+              docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
+                  app.push("${env.BUILD_NUMBER}")
+                  app.push("latest")
+                  }
+                      echo "Push Docker Build to Proget"
+          }
 }
