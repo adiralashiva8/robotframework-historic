431897e6ef4857280a9552a6bfe88322d05e5eff
Tommy H <hoangx@users.noreply.github.com>
2020-04-17 14:31:39 -0500

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,5 +10,16 @@ pipeline {
                         }}
                     }
                 }
+				    stage('Build image') {
+					app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
+					}
+
+					stage('Push image') {
+						docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
+						app.push("${env.BUILD_NUMBER}")
+						app.push("latest")
+					}
+					echo "Trying to Push Docker Build to Proget"
+					}	
             }
         }
