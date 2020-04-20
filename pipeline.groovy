d6b2e25cd205ac1e8fef6b280dcefa58cd8aa054
Tommy H <hoangx@users.noreply.github.com>
2020-04-20 12:31:36 -0500

    revert back to local pipeline run

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,32 +1,25 @@
-pipeline {
-    agent {label 'tommy_test'}
-    stages {
-                stage("Pull Repository") {
-                    steps {
-                        script{container('qe-dockerr') {
-                        git branch: 'master', url: 'https://github.com/Accruent/robotframework-historic.git'
-                        }}
-                    }
-                }
-	    
-		stage('Build image') {
-                    	steps {
-				script{container('qe-dockerr') {
-				def app	
-				app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
-				}}
-			}
-		}
+node {
+    def app
 
-		stage('Push image') {
-                    	steps {
-				script{container('qe-dockerr') {
-				docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
-				app.push("${env.BUILD_NUMBER}")
-				app.push("latest")
-			}}	
-            }
+    stage('Clone repository') {
+        checkout scm
+    }
+
+    stage('Build image') {
+        app = docker.build("proget.accruentsystems.com/qe_docker/rfhistoric")
+    }
+/*
+    stage('Test image') {
+        app.inside {
+            echo "Tests passed"
         }
-	}
-	}
-	}
+    }
+*/
+    stage('Push image') {
+        docker.withRegistry('http://proget.accruentsystems.com/qe_docker/', 'svcselenium') {
+            app.push("${env.BUILD_NUMBER}")
+            app.push("latest")
+            }
+                echo "Trying to Push Docker Build to Proget"
+    }
+}
