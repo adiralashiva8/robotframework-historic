0a7950f47ba8ea6fa700c90ca42558041c8e5f61
Tommy H <tommy.hoang@accruent.com>
2020-05-15 11:18:39 -0500

    Revert "change db to account, home page"
    This reverts commit 580a18b83a1d257f951824c5048917216a0cf001.

--- robotframework_historic/app.py
+++ robotframework_historic/app.py
@@ -39,7 +39,7 @@ def login():
         password = request.form['password'].encode('utf-8')
 
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        use_db(curl, "accounts")
+        use_db(curl, "robothistoric")
         curl.execute("SELECT * FROM TB_USERS WHERE email=%s",(email,))
         user = curl.fetchone()
         curl.close()
@@ -48,18 +48,18 @@ def login():
             if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
                 session['name'] = user['name']
                 session['email'] = user['email']
-                return render_template("/")
+                return render_template("index.html")
             else:
                 return "Error password and email not match"
         else:
             return "Error user not found"
     else:
-        return render_template("/")
+        return render_template("login.html")
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
     session.clear()
-    return render_template("/")
+    return render_template("index.html")
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -72,12 +72,12 @@ def register():
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
-        use_db(cur, "accounts")
+        use_db(cur, "robothistoric")
         cur.execute("INSERT INTO TB_USERS (name, email, password) VALUES (%s,%s,%s)",(name,email,hash_password,))
         mysql.connection.commit()
         session['name'] = request.form['name']
         session['email'] = request.form['email']
-        return redirect(url_for('/'))
+        return redirect(url_for('index'))
 
 @app.route('/newdb', methods=['GET', 'POST'])
 def add_db():
@@ -334,7 +334,6 @@ def main():
     app.config['MYSQL_PORT'] = int(args.sqlport)
     app.config['MYSQL_USER'] = args.username
     app.config['MYSQL_PASSWORD'] = args.password
-    # This Breaks EVERYTHING
     # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     app.config['auth_plugin'] = 'mysql_native_password'
     app.secret_key = "^A%DJAJU^JJ321"
