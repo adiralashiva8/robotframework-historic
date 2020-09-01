2f305a45e370055d86948c13472cb986c6537fff
Shiva <adiralashiva8@gmail.com>
2020-09-01 12:36:19 +0530

    Include asset

--- robotframework_historic/static/assets/js/dashboard.js
+++ robotframework_historic/static/assets/js/dashboard.js
@@ -9,16 +9,16 @@ shim: {
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
-'core': 'js/core',
-'jquery': 'js/vendors/jquery-3.2.1.min',
-'bootstrap': 'js/vendors/bootstrap.bundle.min',
-'sparkline': 'js/vendors/jquery.sparkline.min',
-'selectize': 'js/vendors/selectize.min',
-'tablesorter': 'js/vendors/jquery.tablesorter.min',
-'vector-map': 'js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': 'js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': 'js/vendors/jquery-jvectormap-world-mill',
-'circle-progress': 'js/vendors/circle-progress.min',
+'core': 'assets/js/core',
+'jquery': 'assets/js/vendors/jquery-3.2.1.min',
+'bootstrap': 'assets/js/vendors/bootstrap.bundle.min',
+'sparkline': 'assets/js/vendors/jquery.sparkline.min',
+'selectize': 'assets/js/vendors/selectize.min',
+'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
+'vector-map': 'assets/js/vendors/jquery-jvectormap-2.0.3.min',
+'vector-map-de': 'assets/js/vendors/jquery-jvectormap-de-merc',
+'vector-map-world': 'assets/js/vendors/jquery-jvectormap-world-mill',
+'circle-progress': 'assets/js/vendors/circle-progress.min',
 }
 });
 window.tabler = {
