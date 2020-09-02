449ad1525af0c60b4afc6ab716f88a45cc457c68
Shiva <adiralashiva8@gmail.com>
2020-09-02 09:49:42 +0530

    Apex charts plugin changes

--- robotframework_historic/templates/assets/js/dashboard.js
+++ robotframework_historic/templates/assets/js/dashboard.js
@@ -2,7 +2,7 @@ require.config({
 shim: {
 'bootstrap': ['jquery'],
 'tablesorter': ['jquery'],
-'apexchart': ['jquery'],
+'apexcharts': ['jquery'],
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
@@ -12,7 +12,7 @@ paths: {
 'selectize': 'assets/js/vendors/selectize.min',
 'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
 'circle-progress': 'assets/js/vendors/circle-progress.min',
-'apexchart': 'assets/js/vendors/apexchart',
+'apexcharts': 'assets/js/vendors/apexcharts.min',
 }
 });
 window.tabler = {
---
+++ robotframework_historic/templates/assets/plugins/apexcharts/plugin.js
@@ -0,0 +1,8 @@
+require.config({
+    shim: {
+        'apexcharts': ['jquery','core'],
+    },
+    paths: {
+        'apexcharts': 'assets/plugins/apexcharts/apexcharts.min',
+    }
+});
