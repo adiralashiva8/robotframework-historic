67507bd8ac0aa2f1d5da717e273107fcb4546d9f
Shiva <adiralashiva8@gmail.com>
2020-09-02 09:40:37 +0530

    Added apexchart js files

--- robotframework_historic/templates/assets/js/dashboard.js
+++ robotframework_historic/templates/assets/js/dashboard.js
@@ -1,24 +1,18 @@
 require.config({
 shim: {
 'bootstrap': ['jquery'],
-'sparkline': ['jquery'],
 'tablesorter': ['jquery'],
-'vector-map': ['jquery'],
-'vector-map-de': ['vector-map', 'jquery'],
-'vector-map-world': ['vector-map', 'jquery'],
+'apexchart': ['jquery'],
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
 'core': 'assets/js/core',
 'jquery': 'assets/js/vendors/jquery-3.2.1.min',
 'bootstrap': 'assets/js/vendors/bootstrap.bundle.min',
-'sparkline': 'assets/js/vendors/jquery.sparkline.min',
 'selectize': 'assets/js/vendors/selectize.min',
 'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
-'vector-map': 'assets/js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': 'assets/js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': 'assets/js/vendors/jquery-jvectormap-world-mill',
 'circle-progress': 'assets/js/vendors/circle-progress.min',
+'apexchart': 'assets/js/vendors/apexchart',
 }
 });
 window.tabler = {
