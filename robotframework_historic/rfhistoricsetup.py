36c279f2ae7d317375fa92bd28e31722782d77ed
Shiva <adiralashiva8@gmail.com>
2020-09-19 19:47:54 +0530

    Handle execptions

--- robotframework_historic/rfhistoricsetup.py
+++ robotframework_historic/rfhistoricsetup.py
@@ -5,7 +5,7 @@ def rfhistoric_setup(opts):
 
     # connect to database
     print("INFO: Connecting to dB")
-    mydb = connect_to_mysql_db(opts.host, opts.username, opts.password)
+    mydb = connect_to_mysql(opts.host, opts.username, opts.password)
 
     # create new user
     obj = mydb.cursor()
@@ -14,35 +14,35 @@ def rfhistoric_setup(opts):
     try:
         obj.execute("CREATE USER IF NOT EXISTS 'superuser'@'localhost' IDENTIFIED BY 'passw0rd';")
         obj.execute("GRANT ALL PRIVILEGES ON *.* TO 'superuser'@'localhost' WITH GRANT OPTION;")
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
     
     print("INFO: Creating superuser with remote access")
     try:
         obj.execute("CREATE USER 'superuser'@'%' IDENTIFIED BY 'passw0rd';")
         obj.execute("GRANT ALL PRIVILEGES ON *.* TO 'superuser'@'%' WITH GRANT OPTION;")
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
     
     print("INFO: Reloading grant table")
     try:
         obj.execute("FLUSH PRIVILEGES;")
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
     
     print("INFO: Creating robothistoric dB")
     try:
         obj.execute("CREATE DATABASE IF NOT EXISTS robothistoric;")
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
 
     print("INFO: Creating TB_PROJECT table")
     rfdb = connect_to_mysql_db(opts.host, opts.username, opts.password, "robothistoric")
     try:
         rfdb.execute("CREATE TABLE IF NOT EXISTS TB_PROJECT ( Project_Id INT NOT NULL auto_increment primary key, Project_Name TEXT, Project_Desc TEXT, Project_Image TEXT, Created_Date DATETIME, Last_Updated DATETIME, Total_Executions INT, Recent_Pass_Perc FLOAT, Overall_Pass_Perc FLOAT);")
         rfdb.commit
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
 
     commit_and_close_db(mydb)
 
